21efed87148fc1f2ae65e5bcce0fa053abc5bce1
Sebastian Landwehr <info@dword-design.de>
2021-04-26 19:33:20 +0200

    fix: hide caret (#34)

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,73 @@
-import { endent } from '@dword-design/functions'
+import { endent, map, range } from '@dword-design/functions'
 import packageName from 'depcheck-package-name'
 import execa from 'execa'
+import { readFile } from 'fs-extra'
 import outputFiles from 'output-files'
 import unifyMochaOutput from 'unify-mocha-output'
 import withLocalTmpDir from 'with-local-tmp-dir'
 
 export default {
+  caret() {
+    return withLocalTmpDir(async () => {
+      await outputFiles({
+        pages: {
+          'foo.vue': endent`
+            <template>
+              <input type="text" />
+            </template>
+
+          `,
+          'index.spec.js': endent`
+            import tester from '${packageName`@dword-design/tester`}'
+            import { Builder, Nuxt } from '${packageName`nuxt`}'
+            import { delay, filter, map, range } from '@dword-design/functions'
+            import self from '../../src'
+
+            export default tester({
+              async works() {
+                const nuxt = new Nuxt({ dev: true })
+                await new Builder(nuxt).build()
+                try {
+                  await nuxt.listen()
+                  await this.page.goto('http://localhost:3000')
+                  let input = await this.page.waitForSelector('input[type=text]')
+                  await input.evaluate(el => el.focus())
+                  const shot = (start = 0) => async index => {
+                    await delay(200)
+                    await this.page.screenshot({ path: \`screenshot\${start + index}.png\` })
+                  }
+                  await (range(5) |> map(shot()) |> Promise.all)
+                  await this.page.goto('http://localhost:3000/foo')
+                  input = await this.page.waitForSelector('input[type=text]')
+                  await input.evaluate(el => el.focus())
+                  await (range(5) |> map(shot(5)) |> Promise.all)
+                } finally {
+                  nuxt.close()
+                }
+              }
+            }, [self()])
+
+          `,
+          'index.vue': endent`
+            <template>
+              <input type="text" />
+            </template>
+
+          `,
+        },
+      })
+      await execa.command(
+        `mocha --ui ${packageName`mocha-ui-exports-auto-describe`} --timeout 80000 pages/index.spec.js`
+      )
+      await (range(10)
+        |> map(async index =>
+          expect(
+            readFile(`screenshot${index}.png`) |> await
+          ).toMatchImageSnapshot(this)
+        )
+        |> Promise.all)
+    })
+  },
   chdir() {
     return withLocalTmpDir(async () => {
       await outputFiles({
