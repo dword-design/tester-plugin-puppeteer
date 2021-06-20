59a5b181af9e07d31578ed807de2c915b8787108
dword <info@sebastianlandwehr.com>
2021-06-20 13:46:53 +0000

    fix: xvfb

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
 import puppeteer from '@dword-design/puppeteer'
+import isDocker from 'is-docker'
 import P from 'path'
 import puppeteerToIstanbul from 'puppeteer-to-istanbul'
+import Xvfb from 'xvfb'
+
+const xvfb = new Xvfb()
 
 const storagePath = P.resolve('.nyc_output')
 
-export default (options = {}) => ({
-  after() {
-    puppeteerToIstanbul.write(this.puppeteerCoverages, { storagePath })
-  },
-  async afterEach() {
-    this.puppeteerCoverages.push(...(await this.page.coverage.stopJSCoverage()))
-    this.puppeteerCoverages.push(
-      ...(await this.page.coverage.stopCSSCoverage())
-    )
-    await this.page.close()
-    await this.browser.close()
-  },
-  before() {
-    this.puppeteerCoverages = []
-  },
-  async beforeEach() {
-    this.browser = await puppeteer.launch(options.launchOptions)
-    this.page = await this.browser.newPage()
-    this.page.on('framenavigated', () =>
-      this.page.addStyleTag({ content: '* { caret-color: transparent }' })
-    )
-    await Promise.all([
-      this.page.coverage.startJSCoverage(),
-      this.page.coverage.startCSSCoverage(),
-    ])
-  },
-})
+export default (options = {}) => {
+  options = { launchOptions: {}, ...options }
+
+  const useXvfb = !options.launchOptions.headless && isDocker()
+
+  return {
+    after() {
+      puppeteerToIstanbul.write(this.puppeteerCoverages, { storagePath })
+    },
+    async afterEach() {
+      if (this.page) {
+        this.puppeteerCoverages.push(
+          ...(await this.page.coverage.stopJSCoverage())
+        )
+        this.puppeteerCoverages.push(
+          ...(await this.page.coverage.stopCSSCoverage())
+        )
+        await this.page.close()
+      }
+      if (this.browser) {
+        await this.browser.close()
+      }
+      if (useXvfb) {
+        xvfb.stopSync()
+      }
+    },
+    before() {
+      this.puppeteerCoverages = []
+    },
+    async beforeEach() {
+      if (useXvfb) {
+        xvfb.startSync()
+      }
+      this.browser = await puppeteer.launch(options.launchOptions)
+      this.page = await this.browser.newPage()
+      this.page.on('framenavigated', () =>
+        this.page.addStyleTag({ content: '* { caret-color: transparent }' })
+      )
+      await Promise.all([
+        this.page.coverage.startJSCoverage(),
+        this.page.coverage.startCSSCoverage(),
+      ])
+    },
+  }
+}
