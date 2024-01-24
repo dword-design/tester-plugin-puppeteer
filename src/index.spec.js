63437a855db6a4e0a009206022ec284d081ccb52
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-01-24 19:24:23 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -4,56 +4,12 @@ import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
 import packageName from 'depcheck-package-name'
 import { execaCommand } from 'execa'
 import fs from 'fs-extra'
-import outputFiles from 'output-files'
 
 export default tester(
   {
     caret: {
-      files: {
-        pages: {
-          'foo.vue': endent`
-            <template>
-              <input type="text" />
-            </template>
-          `,
-          'index.spec.js': endent`
-            import tester from '${packageName`@dword-design/tester`}'
-            import testerPluginNuxt from '${packageName`@dword-design/tester-plugin-nuxt`}'
-            import { delay, filter, map, range } from '@dword-design/functions'
-
-            import self from '../../src/index.js'
-
-            export default tester({
-              async works() {
-                await this.page.goto('http://localhost:3000')
-                let input = await this.page.waitForSelector('input[type=text]')
-                await input.evaluate(el => el.focus())
-                const shot = (start = 0) => async index => {
-                  await delay(200)
-                  await this.page.screenshot({ path: \`screenshot\${start + index}.png\` })
-                }
-                await (range(5) |> map(shot()) |> Promise.all)
-                await this.page.goto('http://localhost:3000/foo')
-                input = await this.page.waitForSelector('input[type=text]')
-                await input.evaluate(el => el.focus())
-                await (range(5) |> map(shot(5)) |> Promise.all)
-              }
-            }, [testerPluginNuxt(), self()])
-
-          `,
-          'index.vue': endent`
-            <template>
-              <input type="text" />
-            </template>
-
-          `,
-        },
-      },
       async test() {
-        await execaCommand(
-          `mocha --ui exports --timeout 80000 pages/index.spec.js`,
-          { stdio: 'inherit' },
-        )
+        await execaCommand('mocha --ui exports --timeout 80000 index.spec.js')
         await (range(10)
           |> map(async index =>
             expect(
@@ -62,196 +18,216 @@ export default tester(
           )
           |> Promise.all)
       },
+      testFile: endent`
+        import tester from '${packageName`@dword-design/tester`}'
+        import { execaCommand } from 'execa'
+        import kill from '${packageName`tree-kill-promise`}'
+        import { delay, filter, map, range, endent } from '@dword-design/functions'
+        import outputFiles from '${packageName`output-files`}'
+        import P from 'path'
+        import nuxtDevReady from '${packageName`nuxt-dev-ready`}'
+
+        import self from '../src/index.js'
+
+        export default tester({
+          async works() {
+            await outputFiles({
+              pages: {
+                'index.vue': endent\`
+                  <template>
+                    <input type="text" />
+                  </template>
+                \`,
+                'foo.vue': endent\`
+                  <template>
+                    <input type="text" />
+                  </template>
+                \`,
+              },
+            })
+            const nuxt = execaCommand('${packageName`nuxt`} dev')
+            try {
+              await nuxtDevReady()
+              await this.page.goto('http://localhost:3000')
+              let input = await this.page.waitForSelector('input[type=text]')
+              await input.evaluate(el => el.focus())
+              const shot = (start = 0) => async index => {
+                await delay(200)
+                await this.page.screenshot({ path: \`screenshot\${start + index}.png\` })
+              }
+              await (range(5) |> map(shot()) |> Promise.all)
+              await this.page.goto('http://localhost:3000/foo')
+              input = await this.page.waitForSelector('input[type=text]')
+              await input.evaluate(el => el.focus())
+              await (range(5) |> map(shot(5)) |> Promise.all)
+            } finally {
+              await kill(nuxt.pid)
+            }
+          }
+        }, [self()])
+      `,
     },
     headful: {
-      files: {
-        'index.spec.js': endent`
-          import tester from '${packageName`@dword-design/tester`}'
-          import self from '../src/index.js'
+      testFile: endent`
+        import tester from '${packageName`@dword-design/tester`}'
+        import self from '../src/index.js'
 
-          export default tester({ works: () => {} }, [self({ launchOptions: { headless: false } })])
-        `,
-      },
-      test: () =>
-        execaCommand('mocha --ui exports --timeout 80000 index.spec.js'),
-    },
-    js: {
-      files: {
-        pages: {
-          'index.js': endent`
-            export default {
-              render: h => <div class="foo">Hello world</div>,
-            }
-          `,
-          'index.spec.js': endent`
-            import tester from '${packageName`@dword-design/tester`}'
-            import testerPluginNuxt from '${packageName`@dword-design/tester-plugin-nuxt`}'
-            import { expect } from '${packageName`expect`}'
-
-            import self from '../../src/index.js'
-
-            export default tester({
-              async works() {
-                await this.page.goto('http://localhost:3000')
-                const $foo = await this.page.waitForSelector('.foo')
-                expect(await $foo.evaluate(el => el.innerText)).toEqual('Hello world')
-              }
-            }, [testerPluginNuxt(), self()])
-          `,
-        },
-      },
-      test: () =>
-        execaCommand('mocha --ui exports --timeout 80000 pages/index.spec.js'),
+        export default tester({ works: () => {} }, [self({ launchOptions: { headless: false } })])
+      `,
     },
     launchOptions: {
-      files: {
-        'index.spec.js': endent`
-          import tester from '${packageName`@dword-design/tester`}'
-
-          import self from '../src/index.js'
-
-          export default tester({
-            works: () => {},
-          }, [self({ launchOptions: { timeout: 1 } })])
-
-        `,
-      },
       test: () =>
         expect(
           execaCommand('mocha --ui exports --timeout 80000 index.spec.js'),
         ).rejects.toThrow(
           'TimeoutError: Timed out after 1 ms while waiting for the WS endpoint URL to appear in stdout!',
         ),
+      testFile: endent`
+        import tester from '${packageName`@dword-design/tester`}'
+
+        import self from '../src/index.js'
+
+        export default tester({
+          works: () => {},
+        }, [self({ launchOptions: { timeout: 1 } })])
+
+      `,
     },
     'multiple tests': {
-      files: {
-        'model/test.js': endent`
-          export default {
-            foo: () => {
-              return 1
+      testFile: endent`
+        import tester from '${packageName`@dword-design/tester`}'
+        import { expect } from '${packageName`expect`}'
+        import nuxtDevReady from '${packageName`nuxt-dev-ready`}'
+        import { execaCommand } from 'execa'
+        import { endent } from '@dword-design/functions'
+        import P from 'path'
+        import fs from 'fs-extra'
+        import kill from 'tree-kill-promise'
+
+        import self from '../src/index.js'
+
+        export default tester({
+          async test1() {
+            await this.page.goto('http://localhost:3000')
+            await this.page.evaluate(() => localStorage.setItem('foo', 'bar'))
+          },
+          async test2() {
+            await this.page.goto('http://localhost:3000')
+            expect(await this.page.evaluate(() => localStorage.getItem('foo'))).toBeNull()
+          },
+        }, [
+          {
+            async before() {
+              await fs.outputFile(P.join('pages', 'index.vue'), endent\`
+                <template>
+                  <div />
+                </template>
+              \`)
+              this.nuxt = execaCommand('${packageName`nuxt`} dev')
+              await nuxtDevReady()
             },
-            bar: () => {
-              return 2
+            async after() {
+              await kill(this.nuxt.pid)
             }
-          }
-        `,
-        pages: {
-          '1.js': endent`
-            import test from '../model/test'
+          },
+          self(),
+        ])
 
-            export default {
-              render: h => <div class="foo">{test.foo()}</div>,
-            }
-
-          `,
-          '2.js': endent`
-            import test from '../model/test'
-
-            export default {
-              render: h => <div class="foo">{test.bar()}</div>,
-            }
-
-          `,
-          'index.spec.js': endent`
-            import tester from '${packageName`@dword-design/tester`}'
-            import testerPluginNuxt from '${packageName`@dword-design/tester-plugin-nuxt`}'
-            import { expect } from '${packageName`expect`}'
-            import self from '../../src/index.js'
-
-            export default tester({
-              async test1() {
-                await this.page.goto('http://localhost:3000/1')
-                await this.page.evaluate(() => localStorage.setItem('foo', 'bar'))
-              },
-              async test2() {
-                await this.page.goto('http://localhost:3000/2')
-                expect(await this.page.evaluate(() => localStorage.getItem('foo'))).toBeNull()
-              }
-            }, [self(), testerPluginNuxt()])
-
-          `,
-        },
-      },
-      test: () =>
-        execaCommand('mocha --ui exports --timeout 80000 pages/index.spec.js'),
+      `,
     },
     sass: {
-      files: {
-        'assets/style.scss': endent`
-          .foo {
-            color: red;
+      testFile: endent`
+        import tester from '${packageName`@dword-design/tester`}'
+        import { expect } from '${packageName`expect`}'
+        import outputFiles from '${packageName`output-files`}'
+        import { execaCommand } from 'execa'
+        import kill from 'tree-kill-promise'
+        import { endent } from '@dword-design/functions'
+        import nuxtDevReady from 'nuxt-dev-ready'
+
+        import self from '../src/index.js'
+
+        export default tester({
+          async works() {
+            await outputFiles({
+              'assets/style.scss': endent\`
+                .foo {
+                  color: red;
+                }
+      
+                .bar {
+                  color: green;
+                }
+              \`,
+              'nuxt.config.js': endent\`
+                export default {
+                  css: ['./assets/style.scss'],
+                }
+              \`,
+              'pages/index.vue': endent\`
+                <template>
+                  <div class="foo" />
+                </template>
+              \`,
+            })
+            const nuxt = execaCommand('${packageName`nuxt`} dev')
+            try {
+              await nuxtDevReady()
+              await this.page.goto('http://localhost:3000')
+              const $foo = await this.page.waitForSelector('.foo')
+              expect(await $foo.evaluate(el => window.getComputedStyle(el).color)).toEqual('rgb(255, 0, 0)')
+            } finally {
+              await kill(nuxt.pid)
+            }
           }
-
-          .bar {
-            color: green;
-          }
-
-        `,
-        pages: {
-          'index.spec.js': endent`
-            import tester from '${packageName`@dword-design/tester`}'
-            import testerPluginNuxt from '${packageName`@dword-design/tester-plugin-nuxt`}'
-            import { expect } from '${packageName`expect`}'
-            import self from '../../src/index.js'
-
-            export default tester({
-              async works() {
-                await this.page.goto('http://localhost:3000')
-                const $foo = await this.page.waitForSelector('.foo')
-                expect(await $foo.evaluate(el => el.innerText)).toEqual('Hello world')
-              }
-            }, [testerPluginNuxt(), self()])
-
-          `,
-          'index.vue': endent`
-            <template>
-              <div class="foo">Hello world</div>
-            </template>
-
-          `,
-        },
-      },
-      test: () =>
-        execaCommand('mocha --ui exports --timeout 80000 pages/index.spec.js'),
+        }, [self()])
+      `,
     },
     vue: {
-      files: {
-        pages: {
-          'index.spec.js': endent`
-            import tester from '${packageName`@dword-design/tester`}'
-            import testerPluginNuxt from '${packageName`@dword-design/tester-plugin-nuxt`}'
-            import { expect } from '${packageName`expect`}'
+      testFile: endent`
+        import tester from '${packageName`@dword-design/tester`}'
+        import fs from 'fs-extra'
+        import { expect } from '${packageName`expect`}'
+        import { execaCommand } from 'execa'
+        import { endent } from '@dword-design/functions'
+        import P from 'path'
+        import kill from 'tree-kill-promise'
+        import nuxtDevReady from 'nuxt-dev-ready'
 
-            import self from '../../src/index.js'
+        import self from '../src/index.js'
 
-            export default tester({
-              async works() {
-                await this.page.goto('http://localhost:3000')
-                const $foo = await this.page.waitForSelector('.foo')
-                expect(await $foo.evaluate(el => el.innerText)).toEqual('Hello world')
-              }
-            }, [testerPluginNuxt(), self()])
-
-          `,
-          'index.vue': endent`
-            <template>
-              <div class="foo">Hello world</div>
-            </template>
-
-          `,
-        },
-      },
-      test: () =>
-        execaCommand('mocha --ui exports --timeout 80000 pages/index.spec.js'),
+        export default tester({
+          async works() {
+            await fs.outputFile(P.join('pages', 'index.vue'), endent\`
+              <template>
+                <div class="foo">Hello world</div>
+              </template>
+            \`)
+            const nuxt = execaCommand('${packageName`nuxt`} dev')
+            try {
+              await nuxtDevReady()
+              await this.page.goto('http://localhost:3000')
+              const $foo = await this.page.waitForSelector('.foo')
+              expect(await $foo.evaluate(el => el.innerText)).toEqual('Hello world')
+            } finally {
+              await kill(nuxt.pid)
+            }
+          }
+        }, [self()])
+      `,
     },
   },
   [
     testerPluginTmpDir(),
     {
-      transform: test =>
+      transform: ({
+        testFile,
+        test = () =>
+          execaCommand('mocha --ui exports --timeout 80000 index.spec.js'),
+      }) =>
         async function () {
-          await outputFiles(test.files)
-          await test.test.call(this)
+          await fs.outputFile('index.spec.js', testFile)
+          await test.call(this)
         },
     },
   ],
